b55c93d46e17adb8c4856860042678294ebbfcca
Rafael Zingano <confrariaweb@gmail.com>
2021-01-21 14:06:20 -0300

    Update all

--- src/Views/sections/contact.blade.php
+++ src/Views/sections/contact.blade.php
@@ -7,26 +7,26 @@
         <div class="row">
             <div class="col-lg-12">
                 <form autocomplete="false" role="form" method="post" action="https://mautic.confrariaweb.com.br/form/submit?formId=1" id="mauticform_contatositeconfrariaweb" data-mautic-form="contatositeconfrariaweb" enctype="multipart/form-data">
-                    <div class="mauticform-error" id="mauticform_contatoconfrariaweb_error"></div>
-                    <div class="mauticform-message alert" id="mauticform_contatoconfrariaweb_message"></div>
+                    <div class="mauticform-error" id="mauticform_contatositeconfrariaweb_error"></div>
+                    <div class="mauticform-message alert" id="mauticform_contatositeconfrariaweb_message"></div>
                     <div class="row">
                         <div class="col-xs-12 col-lg-5">
-                            <input id="mauticform_input_contatoconfrariaweb_seu_nome" name="mauticform[seu_nome]" value="" size="40" class="contact-text mb-10 mauticform-input" type="text" placeholder="Seu nome">
+                            <input id="mauticform_input_contatositeconfrariaweb_seu_nome" name="mauticform[seu_nome]" value="" size="40" class="contact-text mb-10 mauticform-input" type="text" placeholder="Seu nome">
                             <span class="mauticform-errormsg" style="display: none;"></span>
                             <br>
-                            <input id="mauticform_input_contatoconfrariaweb_seu_email" name="mauticform[seu_email]" value="" size="40" class="contact-text mb-10 mt-10 mauticform-input" placeholder="Seu e-mail" type="email">
+                            <input id="mauticform_input_contatositeconfrariaweb_seu_email" name="mauticform[seu_email]" value="" size="40" class="contact-text mb-10 mt-10 mauticform-input" placeholder="Seu e-mail" type="email">
                             <span class="mauticform-errormsg" style="display: none;"></span>
                             <br>
-                            <input id="mauticform_input_contatoconfrariaweb_seu_telefone" name="mauticform[seu_telefone]" value="" size="40" class="contact-text mb-10 mt-10 mauticform-input" type="tel" placeholder="Seu telefone?">
+                            <input id="mauticform_input_contatositeconfrariaweb_seu_telefone" name="mauticform[seu_telefone]" value="" size="40" class="contact-text mb-10 mt-10 mauticform-input" type="tel" placeholder="Seu telefone?">
                             <span class="mauticform-errormsg" style="display: none;"></span>
                             <br>
                             <div class="m-10"></div>
                         </div>
                         <div class="col-xs-12 col-lg-7">
-                            <textarea id="mauticform_input_contatoconfrariaweb_mensagem" name="mauticform[mensagem]" cols="40" rows="10" class="contact-message mb-10 mauticform-textarea" placeholder="Mensagem"></textarea>
+                            <textarea id="mauticform_input_contatositeconfrariaweb_mensagem" name="mauticform[mensagem]" cols="40" rows="10" class="contact-message mb-10 mauticform-textarea" placeholder="Mensagem"></textarea>
                             <span class="mauticform-errormsg" style="display: none;"></span>
                             <br>
-                            <button type="submit" name="mauticform[submit]" id="mauticform_input_contatoconfrariaweb_submit" value="" class="send-btn mt-20 mauticform-button btn btn-default">ENVIAR A MENSAGEM</button>
+                            <button type="submit" name="mauticform[submit]" id="mauticform_input_contatositeconfrariaweb_submit" value="" class="send-btn mt-20 mauticform-button btn btn-default">ENVIAR A MENSAGEM</button>
                         </div>
                     </div>
                     <input type="hidden" name="mauticform[formId]" id="mauticform_contatositeconfrariaweb_id" value="1">
